766880ae816d2b7284d028b48f5832c57aea6ff6
Parmar Vishal Karmshibhai <123800206+parmarvishal31@users.noreply.github.com>
2024-02-23 13:56:31 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,9 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store.js";
 import { Toaster } from "react-hot-toast";
-const baseUrl = process.env.REACT_APP_BASE_URL;
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename={baseUrl}>
+    <BrowserRouter >
       <Provider store={store}>
         <App />
         <Toaster position="top-center" reverseOrder={false} />
